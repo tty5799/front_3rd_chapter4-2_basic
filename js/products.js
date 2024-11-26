c4aaa031373023b0ee2323f44caf54d8301718a7
윤성용 <iop017779@gmail.com>
2024-11-27 00:02:00 +0900

    refactor : products image lazy loading 적용

--- js/products.js
+++ js/products.js
@@ -23,6 +23,7 @@ function displayProducts(products) {
         img.src = product.image;
         img.alt = `product: ${product.title}`;
         img.width=250;
+        img.loading="lazy";
         pictureDiv.appendChild(img);
 
         // Create the product info div
